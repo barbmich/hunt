e44c4024fac83ae2452128c9f668c1cf6111bb4e
Michele <barbmich.since86@gmail.com>
2020-10-13 13:52:27 -0400

    routing and tabs

---
+++ src/pages/Product.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { IonHeader, IonPage, IonToolbar, IonTitle } from "@ionic/react";
+
+const Product = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Product</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default Product;
