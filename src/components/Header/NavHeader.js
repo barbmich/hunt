05b4e1ada5751b8c34b8e2617335d617826be5f0
Michele <barbmich.since86@gmail.com>
2020-10-14 14:52:42 -0400

    Product Page

--- src/components/Header/NavHeader.js
+++ src/components/Header/NavHeader.js
@@ -3,17 +3,26 @@ import {
   IonBackButton,
   IonButton,
   IonHeader,
+  IonIcon,
   IonTitle,
   IonToolbar,
+  IonButtons,
 } from "@ionic/react";
 
-const NavHeader = ({ title }) => {
+const NavHeader = ({ title, option, icon, action }) => {
   return (
     <IonHeader>
       <IonToolbar color="primary">
         <IonButton slot="start">
           <IonBackButton defaultHref="/" />
         </IonButton>
+        {option && (
+          <IonButtons slot="primary">
+            <IonButton onClick={action}>
+              <IonIcon slot="icon-only" icon={icon} />
+            </IonButton>
+          </IonButtons>
+        )}
         <IonTitle>{title}</IonTitle>
       </IonToolbar>
     </IonHeader>
